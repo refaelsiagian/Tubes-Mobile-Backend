539f30095960c58fa40ad98d02ea6cf834dae36d
Yuna <yunadhuha123@gmail.com>
2025-12-07 12:09:29 +0700

    add name

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('username', 50)->unique();
+            $table->string('name', 100);
             $table->string('email', 100)->unique();
             $table->string('password');
             $table->text('bio')->nullable();
